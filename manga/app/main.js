2cdad07b97314f3ce98040abe7c623a8692e314d
AravinthanN <aravinthankpt@gmail.com>
2024-11-18 20:21:12 +0530

    g

--- manga/app/main.js
+++ manga/app/main.js
@@ -4,7 +4,7 @@ const { useState } = React;
 const ReactDOM = window.ReactDOM;
 
 // Import the Home component
-import { Home } from "./page/Home"; // Assuming Home component is in 'page/Home.js'
+import { Home } from "./page"; // Assuming Home component is in 'page/Home.js'
 
 // Render the Home component into the 'root' element
 ReactDOM.render(<Home />, document.getElementById("root"));
