0d67f218de8af6c1506d05d594f301d12526ca9a
AravinthanN <159110239+AravinthanN@users.noreply.github.com>
2024-11-18 21:19:26 +0530

    Update main-compiled.js

--- manga/app/main-compiled.js
+++ manga/app/main-compiled.js
@@ -2,9 +2,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Home } from "./page"; // Assuming Home component is in './page'
-
+console.log("cumpdhgfngfdli");
 // Get the root element
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log("cumpli");
+console.log("cumpdhdli");
 // Render your Home component
 root.render(/*#__PURE__*/ React.createElement(Home, null));
