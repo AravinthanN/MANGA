909bb9c2222b8df80b12e5395714c4dd517e303b
AravinthanN <aravinthankpt@gmail.com>
2024-11-18 20:54:17 +0530

    dfh

---
+++ manga/app/main-compiled.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Home } from "./page"; // Assuming Home component is in './page'
+
+// Get the root element
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render your Home component
+root.render(/*#__PURE__*/ React.createElement(Home, null));
